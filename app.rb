3494c9b32c624c6bff95d492ddb62ee7eb80df3c
cauecjm <cauecjm@gmail.com>
2019-09-13 18:28:28 -0300

    add configs file (rakefile, app.rb, config,ru, dockerfile, docker-compose.yml)

---
+++ app.rb
@@ -0,0 +1,35 @@
+require 'json'
+require 'sinatra'
+
+# Load Services
+Dir["./app/services/**/*.rb"].each {|file| require file }
+
+class App < Sinatra::Base
+
+  get '/' do
+    'NazuSoft Bit Translate! (en-pt <-> pt-en)'
+  end
+
+  post '/webhook' do
+    request.body.rewind
+    result = JSON.parse(request.body.read)['queryResult']
+
+    params   = result['contexts'].present? ? result['contexts'][0]['parameters'] : result['parameters']
+    response = TranslateService.perform(result['action'], params)
+
+    content_type :json, charset: 'utf-8'
+    {
+      'fulfillmentMessages': [
+        {
+          'payload': {
+            'telegram': {
+              'text': response,
+              'parse_mode': 'HTML'
+            }
+          }
+        }
+      ]
+    }.to_json
+  end
+
+end
